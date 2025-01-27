89f1586ccd3f2d4a7f25ae5ff9ca69d3f2ed738d
BugHunter-X <askzenai@gmail.com>
2025-01-27 13:22:06 +0100

    Add bug.jsx file

---
+++ bug.jsx
@@ -0,0 +1,19 @@
+```javascript
+// MyComponent.jsx
+
+function MyComponent() {
+  return (
+    <div className="bg-gray-100 p-4">
+      <div className="flex">
+        <div className="w-1/2">
+          <p>Some Text</p>
+        </div>
+        <div className="w-1/2">
+          <p>Some More Text</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+```
